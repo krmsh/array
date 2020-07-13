feac4c4fea4852ef840f1e1d8032d788bc7feead
nami <krmglish@gmail.com>
2020-07-13 22:09:12 +0900

    array

---
+++ array.rb
@@ -0,0 +1,13 @@
+languages = ["Ruby", "PHP", "Java"]
+
+puts "様々な言語のHello World\n"
+puts 
+
+languages.each do |language|
+  if language == "Ruby"
+    puts "#{language}:puts \"Hello World!\""
+  elsif language == "PHP"
+    puts "#{language}:echo \"Hello World!\";"
+  else language == "Java"
+    puts "#{language}:System.out.println(\"Hello World!\");"   end
+end
